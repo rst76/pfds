8bb9f2fbbeb898616b17c65684e742433eea5b2b
Ryosuke TAKASHIMA <ryosuke.takashima@gmail.com>
2017-07-09 23:24:10 +0900

    Solved 3.7. (work in prgress)

---
+++ ch03/ExplicitMinHeap.hs
@@ -0,0 +1,30 @@
+module ExplicitMinHeap where
+
+import qualified Heap as H
+
+data ExplicitMinHeap h a = E | NE a (h a) deriving Show
+
+instance H.Heap h => H.Heap (ExplicitMinHeap h) where
+
+  empty = E
+
+  isEmpty E = True
+  isEmpty _ = False
+
+  insert x E = NE x (H.insert x H.empty)
+  insert x (NE y h) = NE (min x y) (H.insert x h)
+
+  merge h E = h
+  merge E h = h
+  merge (NE x h1) (NE y h2) = NE (min x y) (H.merge h1 h2)
+
+  findMin E = error "empty heap"
+  findMin (NE x _) = x
+
+  deleteMin E = error "empty heap"
+  deleteMin (NE x h)
+    | H.isEmpty h' = E
+    | otherwise  = NE y h'
+    where
+    h' = H.deleteMin h
+    y  = H.findMin h'
---
+++ ch03/LeftistHeap.hs
@@ -0,0 +1,36 @@
+module LeftistHeap where
+
+import Heap
+
+data LeftistHeap a = E | T Int a (LeftistHeap a) (LeftistHeap a) deriving Show
+
+rank :: LeftistHeap a -> Int
+rank E = 0
+rank (T r _ _ _) = r
+
+makeT :: a -> LeftistHeap a -> LeftistHeap a -> LeftistHeap a
+makeT x a b
+  | rank a >= rank b = T (rank b + 1) x a b
+  | otherwise        = T (rank a + 1) x b a
+
+instance Heap LeftistHeap where
+
+  empty = E
+
+  isEmpty E = True
+  isEmpty _ = False
+
+  insert x h = merge (T 1 x E E) h
+
+  merge h E = h
+  merge E h = h
+  merge h1@(T _ x a1 b1) h2@(T _ y a2 b2)
+    | x <= y    = makeT x a1 (merge b1 h2)
+    | otherwise = makeT y a2 (merge h1 b2)
+
+  findMin E = error "empty heap"
+  findMin (T _ x a b) = x
+
+  deleteMin E = error "empty heap"
+  deleteMin (T _ x a b) = merge a b
+  
