20af0d95a6b4a97acb932be80147b5af3626406e
Ryosuke TAKASHIMA <ryosuke.takashima@gmail.com>
2017-07-25 18:27:38 +0900

    Added explanation of lazy evaluation.

--- ch04/ex.4.2.hs
+++ ch04/ex.4.2.hs
@@ -5,8 +5,3 @@ insertionSort = foldl ins []
   ins (y : ys) x
     | x < y = x : y : ys
     | otherwise = y : ins ys x
-
--- To detect each n numbers being the member of the first k numbers,
--- we have to compare at most to the first k members of the sorted list.
--- (It's not necessary to compare to the rest members of the sorted list.)
--- So, sort xs takes O(n * k) time.
