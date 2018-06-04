09f91c35f0bc6ae715538903e07ff0ae83a0573f
Ryosuke TAKASHIMA <ryosuke.takashima@gmail.com>
2018-06-04 11:58:27 +0900

    Modified 9.16.

--- ch09/DeletableHeap.hs
+++ ch09/DeletableHeap.hs
@@ -1,27 +1,27 @@
-module DeletableHeap(DeletableHeap, delete) where
+module DeletableHeap (DeletableHeap, delete) where
 
 import qualified Heap as H
 
 data DeletableHeap h a = DH (h a) (h a) deriving Show
 
 normalize :: Ord a => H.Heap h => DeletableHeap h a -> DeletableHeap h a
-normalize (DH n p)
-  | H.isEmpty n || H.isEmpty p || H.findMin n /= H.findMin p = DH n p
-  | otherwise = normalize (DH (H.deleteMin n) (H.deleteMin p))
+normalize (DH p n)
+  | H.isEmpty p || H.isEmpty n || H.findMin p /= H.findMin n = DH p n
+  | otherwise = normalize (DH (H.deleteMin p) (H.deleteMin n))
 
 delete :: Ord a => H.Heap h => a -> DeletableHeap h a -> DeletableHeap h a
-delete x (DH n p) = normalize (DH (H.insert x n) p)
+delete x (DH p n) = normalize (DH p (H.insert x n))
 
 instance H.Heap h => H.Heap (DeletableHeap h) where
 
   empty = DH H.empty H.empty
 
-  isEmpty (DH n p) = H.isEmpty p
+  isEmpty (DH p n) = H.isEmpty p
 
-  insert x (DH n p) = normalize (DH n (H.insert x p))
+  insert x (DH p n) = normalize (DH (H.insert x p) n)
 
-  merge (DH n1 p1) (DH n2 p2) = normalize (DH (H.merge n1 n2) (H.merge p1 p2))
+  merge (DH p1 n1) (DH p2 n2) = normalize (DH (H.merge p1 p2) (H.merge n1 n2))
 
-  findMin (DH _ p) = H.findMin p
+  findMin (DH p _) = H.findMin p
 
-  deleteMin (DH n p) = normalize (DH n (H.deleteMin p))
+  deleteMin (DH p n) = normalize (DH (H.deleteMin p) n)
--- ch09/ex.9.16.hs
+++ ch09/ex.9.16.hs
@@ -6,10 +6,10 @@ main :: IO ()
 main = do
   let h = delete 3 $ delete 2 $ foldr insert empty [1 .. 5] :: DeletableHeap SkewBinomialHeap Int
   print h
-  -- DH (SBH [Node 0 3 [] [],Node 0 2 [] []]) (SBH [Node 0 1 [] [],Node 0 2 [] [],Node 1 3 [4] [Node 0 5 [] []]])
+  -- DH (SBH [Node 0 1 [] [],Node 0 2 [] [],Node 1 3 [4] [Node 0 5 [] []]]) (SBH [Node 0 3 [] [],Node 0 2 [] []])
   print $ findMin h
   -- 1
   print $ deleteMin h
-  -- DH (SBH []) (SBH [Node 0 4 [] [],Node 0 5 [] []])
+  -- DH (SBH [Node 0 4 [] [],Node 0 5 [] []]) (SBH [])
   print $ findMin $ deleteMin h 
   -- 4
