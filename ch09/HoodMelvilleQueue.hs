acc5d7bcbed200bf6bff9cfdcc03377a227f0357
Ryosuke TAKASHIMA <ryosuke.takashima@gmail.com>
2018-06-04 16:29:37 +0900

    Modified 9.14.

--- ch09/HoodMelvilleQueue.hs
+++ ch09/HoodMelvilleQueue.hs
@@ -7,24 +7,24 @@ import SkewBinaryRandomAccessList (SkewList)
 
 data RotationState a =
     Idle
-  | Reversing Int (SkewList a) (SkewList a) (SkewList a) (SkewList a)
-  | Appending Int (SkewList a) (SkewList a)
+  | Reversing Int Int (SkewList a) (SkewList a) (SkewList a) (SkewList a)
+  | Appending Int Int (SkewList a) (SkewList a)
   | Done (SkewList a) deriving Show
 
 data HoodMelvilleQueue a = HM Int (SkewList a) (RotationState a) Int (SkewList a) deriving Show
 
 exec :: RotationState a -> RotationState a
-exec (Reversing ok f f' r r')
-  | RL.isEmpty f = Appending ok f' (RL.cons (RL.head r) r')
-  | otherwise = Reversing (ok + 1) (RL.tail f) (RL.cons (RL.head f) f') (RL.tail r) (RL.cons (RL.head r) r')
-exec (Appending 0 _ r') = Done r'
-exec (Appending ok f' r') = Appending (ok - 1) (RL.tail f') (RL.cons (RL.head f') r')
+exec (Reversing ok d f f' r r')
+  | RL.isEmpty f = Appending ok d f' (RL.cons (RL.head r) r')
+  | otherwise = Reversing (ok + 1) d (RL.tail f) (RL.cons (RL.head f) f') (RL.tail r) (RL.cons (RL.head r) r')
+exec (Appending 0 _ _ r') = Done r'
+exec (Appending ok d f' r') = Appending (ok - 1) d (RL.tail f') (RL.cons (RL.head f') r')
 exec state = state
 
 invalidate :: RotationState a -> RotationState a
-invalidate (Reversing ok f f' r r') = Reversing (ok - 1) f f' r r'
-invalidate (Appending 0 _ r') = Done (RL.tail r')
-invalidate (Appending ok f' r') = Appending (ok - 1) f' r'
+invalidate (Reversing ok d f f' r r') = Reversing (ok - 1) (d + 1) f f' r r'
+invalidate (Appending 0 _ _ r') = Done (RL.tail r')
+invalidate (Appending ok d f' r') = Appending (ok - 1) (d + 1) f' r'
 invalidate state = state
 
 exec2 :: Int -> SkewList a -> RotationState a -> Int -> SkewList a -> HoodMelvilleQueue a
@@ -36,42 +36,51 @@ check :: Int -> SkewList a -> RotationState a -> Int -> SkewList a -> HoodMelvil
 check lenf f state lenr r =
   if lenr <= lenf then exec2 lenf f state lenr r
   else exec2 (lenf + lenr) f newstate 0 RL.empty
-  where newstate = exec (Reversing 0 f RL.empty r RL.empty)
+  where newstate = exec (Reversing 0 0 f RL.empty r RL.empty)
 
 lookup :: Int -> HoodMelvilleQueue a -> a
 lookup i (HM lenf _ _ lenr r)
   | i >= lenf = RL.lookup (lenf + lenr - i - 1) r
 lookup i (HM _ f Idle _ _) = RL.lookup i f
-lookup i (HM lenf f state _ _)
-  | i < lenf `div` 2 = RL.lookup i f
-lookup i (HM lenf _ (Appending ok _ r) _ _) = RL.lookup (i - ok) r
-lookup i (HM lenf _ (Reversing ok _ _ rf rr) _ _)
-  | i < lenf - ok = RL.lookup (lenf - ok - i - 1) rf
-  | otherwise = RL.lookup (i - lenf + ok) rr
+lookup i (HM lenf f (Appending ok d _ r) _ _)
+  | i < h     = RL.lookup i f
+  | otherwise = RL.lookup (i - ok) r
+  where
+  h = (lenf - d) `div` 2
+lookup i (HM lenf f (Reversing ok d _ _ rf rr) _ _)
+  | i < h1    = RL.lookup i f
+  | i < h2    = RL.lookup (h2 - i - 1) rf
+  | otherwise = RL.lookup (i - h2) rr
+  where
+  h1 = (lenf - d) `div` 2
+  h2 = lenf - d - ok
 
 update :: Int -> a -> HoodMelvilleQueue a -> HoodMelvilleQueue a
 update i x (HM lenf f state lenr r)
   | i >= lenf = HM lenf f state lenr (RL.update (lenf + lenr - i - 1) x r)
 update i x (HM lenf f Idle lenr r) = HM lenf (RL.update i x f) Idle lenr r
-update i x (HM lenf f (Appending ok ff fr) lenr r)
-  | i < ok           = HM lenf f' (Appending ok ff' fr) lenr r
-  | i < lenf `div` 2 = HM lenf f' (Appending ok ff fr') lenr r
-  | otherwise        = HM lenf f  (Appending ok ff fr') lenr r
+update i x (HM lenf f (Appending ok d ff fr) lenr r)
+  | i < ok    = HM lenf f' (Appending ok d ff' fr) lenr r
+  | i < h     = HM lenf f' (Appending ok d ff fr') lenr r
+  | otherwise = HM lenf f  (Appending ok d ff fr') lenr r
   where
+  h = (lenf - d) `div` 2
   f'  = RL.update i x f
   ff' = RL.update (ok - i - 1) x ff
   fr' = RL.update (i - ok) x fr
-update i x (HM lenf f (Reversing ok fr ff rf rr) lenr r)
-  | i < ok           = HM lenf f' (Reversing ok fr ff' rf rr) lenr r
-  | i < lenf `div` 2 = HM lenf f' (Reversing ok fr' ff rf rr) lenr r
-  | i < lenf - ok    = HM lenf f  (Reversing ok fr ff rf' rr) lenr r
-  | otherwise        = HM lenf f  (Reversing ok fr ff rf rr') lenr r
+update i x (HM lenf f (Reversing ok d fr ff rf rr) lenr r)
+  | i < ok    = HM lenf f' (Reversing ok d fr ff' rf rr) lenr r
+  | i < h1    = HM lenf f' (Reversing ok d fr' ff rf rr) lenr r
+  | i < h2    = HM lenf f  (Reversing ok d fr ff rf' rr) lenr r
+  | otherwise = HM lenf f  (Reversing ok d fr ff rf rr') lenr r
   where
+  h1 = (lenf - d) `div` 2
+  h2 = lenf - d - ok
   f'  = RL.update i x f
   ff' = RL.update (ok - i - 1) x ff
   fr' = RL.update (i - ok) x fr
-  rf' = RL.update (lenf - ok - i - 1) x rf
-  rr' = RL.update (i - lenf + ok) x rr
+  rf' = RL.update (h2 - i - 1) x rf
+  rr' = RL.update (i - h2) x rr
 
 instance Queue HoodMelvilleQueue where
 
--- ch09/ex.9.14.hs
+++ ch09/ex.9.14.hs
@@ -4,19 +4,24 @@ import HoodMelvilleQueue
 
 main :: IO ()
 main = do
-  let ns1 = [0 .. 6]
-  let q1 = foldl snoc empty ns1 :: HoodMelvilleQueue Int
+  let q1 = tail $ tail $ foldl snoc empty [0 .. 13] :: HoodMelvilleQueue Int
   print q1
-  -- HM 7 (SL [(3,Node 0 (Leaf 1) (Leaf 2))]) (Reversing 2 (SL [(1,Leaf 2)]) (SL [(1,Leaf 1),(1,Leaf 0)]) (SL [(1,Leaf 4),(1,Leaf 3)]) (SL [(1,Leaf 5),(1,Leaf 6)])) 0 (SL [])
-  print $ map (`lookup` q1) ns1
-  -- [0,1,2,3,4,5,6]
-  print $ foldl (\q i -> update i (i + 10) q) q1 ns1
-  -- HM 7 (SL [(3,Node 10 (Leaf 11) (Leaf 12))]) (Reversing 2 (SL [(1,Leaf 12)]) (SL [(1,Leaf 11),(1,Leaf 10)]) (SL [(1,Leaf 14),(1,Leaf 13)]) (SL [(1,Leaf 15),(1,Leaf 16)])) 0 (SL [])
-  let ns2 = [0 .. 9]
-  let q2 = foldl snoc empty ns2 :: HoodMelvilleQueue Int
+  -- HM 12 (SL [(1,Leaf 2),(1,Leaf 3),(3,Node 4 (Leaf 5) (Leaf 6))]) (Reversing 2 1 (SL [(3,Node 4 (Leaf 5) (Leaf 6))]) (SL [(3,Node 3 (Leaf 2) (Leaf 1))]) (SL [(1,Leaf 10),(3,Node 9 (Leaf 8) (Leaf 7))]) (SL [(3,Node 11 (Leaf 12) (Leaf 13))])) 0 (SL [])
+  print $ map (`lookup` q1) [0 .. 11]
+  -- [2,3,4,5,6,7,8,9,10,11,12,13]
+  print $ foldr (\i -> update i (i + 10)) q1 [0 .. 11]
+  -- HM 12 (SL [(1,Leaf 10),(1,Leaf 11),(3,Node 12 (Leaf 13) (Leaf 14))]) (Reversing 2 1 (SL [(3,Node 12 (Leaf 13) (Leaf 14))]) (SL [(3,Node 11 (Leaf 10) (Leaf 1))]) (SL [(1,Leaf 18),(3,Node 17 (Leaf 16) (Leaf 15))]) (SL [(3,Node 19 (Leaf 20) (Leaf 21))])) 0 (SL [])
+  let q2 = tail $ tail q1 :: HoodMelvilleQueue Int
   print q2
-  -- HM 7 (SL [(3,Node 0 (Leaf 1) (Leaf 2))]) (Appending 2 (SL [(1,Leaf 1),(1,Leaf 0)]) (SL [(1,Leaf 2),(1,Leaf 3),(3,Node 4 (Leaf 5) (Leaf 6))])) 3 (SL [(3,Node 9 (Leaf 8) (Leaf 7))])
-  print $ map (`lookup` q2) ns2
-  -- [0,1,2,3,4,5,6,7,8,9]
-  print $ foldl (\q i -> update i (i + 10) q) q2 ns2
-  -- HM 7 (SL [(3,Node 10 (Leaf 11) (Leaf 12))]) (Appending 2 (SL [(1,Leaf 11),(1,Leaf 10)]) (SL [(1,Leaf 12),(1,Leaf 13),(3,Node 14 (Leaf 15) (Leaf 16))])) 3 (SL [(3,Node 19 (Leaf 18) (Leaf 17))])
+  -- HM 10 (SL [(3,Node 4 (Leaf 5) (Leaf 6))]) (Reversing 2 3 (SL [(1,Leaf 6)]) (SL [(1,Leaf 5),(1,Leaf 4),(3,Node 3 (Leaf 2) (Leaf 1))]) (SL [(1,Leaf 8),(1,Leaf 7)]) (SL [(1,Leaf 9),(1,Leaf 10),(3,Node 11 (Leaf 12) (Leaf 13))])) 0 (SL [])
+  print $ map (`lookup` q2) [0 .. 9]
+  -- [4,5,6,7,8,9,10,11,12,13]
+  print $ foldr (\i -> update i (i + 10)) q2 [0 .. 9]
+  -- HM 10 (SL [(3,Node 10 (Leaf 11) (Leaf 12))]) (Reversing 2 3 (SL [(1,Leaf 12)]) (SL [(1,Leaf 11),(1,Leaf 10),(3,Node 3 (Leaf 2) (Leaf 1))]) (SL [(1,Leaf 14),(1,Leaf 13)]) (SL [(1,Leaf 15),(1,Leaf 16),(3,Node 17 (Leaf 18) (Leaf 19))])) 0 (SL [])
+  let q3 = tail $ tail q2 :: HoodMelvilleQueue Int
+  print q3
+  -- HM 8 (SL [(1,Leaf 6)]) (Appending 1 5 (SL [(3,Node 6 (Leaf 5) (Leaf 4)),(3,Node 3 (Leaf 2) (Leaf 1))]) (SL [(7,Node 7 (Node 8 (Leaf 9) (Leaf 10)) (Node 11 (Leaf 12) (Leaf 13)))])) 0 (SL [])
+  print $ map (`lookup` q3) [0 .. 7]
+  -- [6,7,8,9,10,11,12,13]
+  print $ foldr (\i -> update i (i + 10)) q3 [0 .. 7]
+  -- HM 8 (SL [(1,Leaf 10)]) (Appending 1 5 (SL [(3,Node 10 (Leaf 5) (Leaf 4)),(3,Node 3 (Leaf 2) (Leaf 1))]) (SL [(7,Node 11 (Node 12 (Leaf 13) (Leaf 14)) (Node 15 (Leaf 16) (Leaf 17)))])) 0 (SL [])
