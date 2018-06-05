9ba8f31c6443dcd62c9303a8a790d7d32054c65c
Ryosuke TAKASHIMA <ryosuke.takashima@gmail.com>
2018-06-05 10:08:35 +0900

    Modified 9.19.

--- ch09/ex.9.19.hs
+++ ch09/ex.9.19.hs
@@ -7,8 +7,10 @@ main = do
   let ns = [0 .. 12]
   let l = foldr cons empty ns :: SkewList Int
   print l
+  -- 001
   -- SL [(13,Node 0 (Node 1 (Leaf 2) (Leaf 3) (Leaf 4)) (Node 5 (Leaf 6) (Leaf 7) (Leaf 8)) (Node 9 (Leaf 10) (Leaf 11) (Leaf 12)))]
   print $ tail l
+  -- 03
   -- SL [(4,Node 1 (Leaf 2) (Leaf 3) (Leaf 4)),(4,Node 5 (Leaf 6) (Leaf 7) (Leaf 8)),(4,Node 9 (Leaf 10) (Leaf 11) (Leaf 12))]
   print $ map (`lookup` l) ns
   -- [0,1,2,3,4,5,6,7,8,9,10,11,12]
